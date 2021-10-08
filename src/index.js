73e3dab9f097dc955576d00a827829de29c336ca
Zeki <zckural@gmail.com>
2021-10-08 14:05:48 -0400

    file structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Example from "./scripts/example"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById("header")
+    new Example(main)
+})
+
---
+++ src/scripts/example.js
@@ -0,0 +1,15 @@
+const d3 = require("d3");
+class Example {
+  constructor(ele){
+      this.ele = ele
+      this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>"
+      this.handleClick = this.handleClick.bind(this)
+      this.ele.addEventListener("click", this.handleClick)
+      d3.select('h1').style('color','darkblue');
+  }
+  
+  handleClick(){
+      this.ele.children[0].innerText = "Ouch!"
+  }
+}
+export default Example;
